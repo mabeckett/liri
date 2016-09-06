aaae3f0d9c3de85241c07f5514da696a8d17f007
Andrew Beckett <Andrew Beckett>
2016-09-06 17:53:57 -0400

    push

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: 'rMlfV24dSh7tkTJ8WPR0eC335',
-  consumer_secret: 'KQROx2KkPLQxnCdafG51E34Ue96UtfbODQKb2eojolalCedH5Z',
-  access_token_key: '448751450-VkadXY5uE9ZKJzyOcVKbKPRiBgiMUa8wEwNXa0dF',
-  access_token_secret: 'L1p1pMMkGGcMvoVh8r7lkJ4ri3ItMauWRhf9GPvboZsfN',
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
 }
