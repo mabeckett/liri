a403541da9fceea5a781b9128d1bf1961ab0a6b0
mabeckett <mabeckett@liberty.edu>
2016-09-06 17:45:36 -0400

    Delete keys.js

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: 'rMlfV24dSh7tkTJ8WPR0eC335',
-  consumer_secret: 'KQROx2KkPLQxnCdafG51E34Ue96UtfbODQKb2eojolalCedH5Z',
-  access_token_key: '448751450-VkadXY5uE9ZKJzyOcVKbKPRiBgiMUa8wEwNXa0dF',
-  access_token_secret: 'L1p1pMMkGGcMvoVh8r7lkJ4ri3ItMauWRhf9GPvboZsfN',
-}
