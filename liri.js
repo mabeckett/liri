968b9c17336dda5192d828a61e5a60799c41991f
Andrew Beckett <Andrew Beckett>
2016-09-06 17:20:05 -0400

    push

--- liri.js
+++ liri.js
@@ -1,36 +1,66 @@
-//file reading//
-var fs = require('fs');
+var action = process.argv[2];
 
-	fs.readFile('random.txt', 'utf8', function(error, data) {
+switch(action) {
 
-	    if (err) {
-	        return console.log(err);
-	    }
-	    console.log('random.txt updated');
-	}); 
-//omdb api//
-var request = require('request');
-var movieName = '';
-var queryUrl = 'http://www.omdbapi.com/?t=' + movieName +'&y=&plot=short&r=json';
+	case 'do-what-it-says':
+		doThis();
+		break;
 
-	console.log(queryUrl);
+	case 'movie-this':
+		movie();
+		break;
 
-	request(queryUrl, function (error, response, body) {
+	case 'my-tweets':
+		tweets();
+		break;
 
-		if (!error && response.statusCode == 200) {
+	case 'spotify-this-song':
+		spotify();
+		break;
 
-			console.log("Release Year: " + JSON.parse(body)["Title"]);
-			console.log("Release Year: " + JSON.parse(body)["Year"]);
-			console.log("Release Year: " + JSON.parse(body)["imdbRating"]);
-			console.log("Release Year: " + JSON.parse(body)["Country"]);
-			console.log("Release Year: " + JSON.parse(body)["Language"]);
-			console.log("Release Year: " + JSON.parse(body)["Plot"]);
-			console.log("Release Year: " + JSON.parse(body)["Actors"]);
-			console.log("Release Year: " + JSON.parse(body)["tomatoRating"]);
-			console.log("Release Year: " + JSON.parse(body)["tomatoURL"]);
-		}
-	});
+	default:
+		console.log('not found')
+		break;
 
+}
+//file reading function//
+function doThis() {
+
+	var fs = require('fs');
+
+		fs.readFile('random.txt', 'utf8', function(error, data) {
+
+		    if (err) {
+		        return console.log(err);
+		    }
+		    console.log('random.txt updated');
+		}); 
+}
+//omdb api function//
+function movie() {
+
+	var request = require('request');
+	var movieName = process.argv[3];
+	var queryUrl = 'http://www.omdbapi.com/?t=' + movieName +'&tomatoes=&y=&plot=short&r=json';
+
+		console.log(queryUrl);
+
+		request(queryUrl, function (error, response, body) {
+
+			if (!error && response.statusCode == 200) {
+
+				console.log("Release Year: " + JSON.parse(body)["Title"]);
+				console.log("Release Year: " + JSON.parse(body)["Year"]);
+				console.log("Release Year: " + JSON.parse(body)["imdbRating"]);
+				console.log("Release Year: " + JSON.parse(body)["Country"]);
+				console.log("Release Year: " + JSON.parse(body)["Language"]);
+				console.log("Release Year: " + JSON.parse(body)["Plot"]);
+				console.log("Release Year: " + JSON.parse(body)["Actors"]);
+				console.log("Release Year: " + JSON.parse(body)["tomatoRating"]);
+				console.log("Release Year: " + JSON.parse(body)["tomatoURL"]);
+			}
+		});
+}
 var twitter = require('twitter');
 
 var spotify = require('spotify');
